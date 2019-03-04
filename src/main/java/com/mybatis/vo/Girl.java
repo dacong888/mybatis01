d808b08dbdf40c1a7126a07cc5527c3ac4863104
songbin <eric_s30@163.com>
2019-03-04 14:59:34 +0800

    commit Girl class

--- src/main/java/com/mybatis/vo/Girl.java
+++ src/main/java/com/mybatis/vo/Girl.java
@@ -1,4 +1,30 @@
 package com.mybatis.vo;
 
 public class Girl {
+    private Integer id;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Girl{" +
+                "id=" + id +
+                ", age=" + age +
+                '}';
+    }
 }
